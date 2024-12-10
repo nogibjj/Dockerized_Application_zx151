7bc33dd9e85c0621b2f51286e97bcfb9a55b3b6f
Zihan Xiao <98075925+zihanxiao23@users.noreply.github.com>
2024-12-10 00:12:00 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello, Dockerized World!"
+def hello_world():
+    return jsonify({"message": "Hello, Dockerized World!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
